1a6d6633e54fc5198449ab0c74daec81e6d745ab
Duc Dinh <holahu9@gmail.com>
2021-03-21 15:05:32 -0400

    html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,26 @@
+// Dependencies
+const path = require('path');
+
+// Routes
+module.exports = (app) => {
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads index.html
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+  }
+  );
+  // route to load exercise.html file
+  app.get('/exercise', (req, res) => {
+    console.log("in exercise html route");
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
+    }
+  );
+  // route to load stats.html file
+  app.get('/stats', (req, res) => {
+    console.log("in stats html route");
+    res.sendFile(path.join(__dirname, '../public/stats.html'))
+    }
+  );
+
+}
