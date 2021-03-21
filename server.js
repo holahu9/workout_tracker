f26d3ad98d806eb4cb116642812d4fe5b8a570b8
Duc Dinh <holahu9@gmail.com>
2021-03-21 15:00:12 -0400

    server

--- server.js
+++ server.js
@@ -5,31 +5,44 @@ const path = require("path");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-const databaseUrl = "notetaker";
-const collections = ["notes"];
-
-const db = mongojs(databaseUrl, collections);
-
-db.on("error", error => {
-  console.log("Database Error:", error);
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 
+const db = require("./models");
+
+// Mongoose connection
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  )  // when connected successfully, a message will be displayed. 
+  .then(() => {
+    console.log("Connected to Mongo database!");
+  })  // error message if failed.
+  .catch(err => {
+    console.error("App starting error:", err.stack);
+  });
+
+
+// routes
+require("./routes/htmlRoutes.js")(app);  // import the html routes
+require("./routes/apiRoutes.js")(app); //import the api routes
 
 
 
 
-
-
-
-
-app.listen(3000, () => {
-    console.log("App running on port 3000!");
+  app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
   });
